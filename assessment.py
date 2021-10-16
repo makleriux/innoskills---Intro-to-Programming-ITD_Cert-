b580b6ac00175d03540e79f92e21439e8dbcdf37
EdenLT <makleriux@gmail.com>
2021-10-16 16:06:13 +0100

    Update assessment.py

--- assessment.py
+++ assessment.py
@@ -4,5 +4,5 @@ import numpy as np
 
 list_values=[5,8,2,3,11,9,19,1,10,7] #hwat should be shorted
 list_values.sort() #sorting array using NumPy
-
-print(list_values) #Print result
+word = "New_sorted_list ="
+print(word, list_values) #Print result
