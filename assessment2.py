a2cfb0f8fe2d92eac44024d03eb48cce52d37922
EdenLT <makleriux@gmail.com>
2021-11-01 22:48:43 +0000

    Create assessment2.py

---
+++ assessment2.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  1 21:45:53 2021
+
+@author: Simas Simanavicius
+Rev.1
+
+Perform addition, subtraction, multiplication, and division of given two variables
+"""
+Var1=9646
+Var2=2783
+
+# var part w operators
+add = Var1+Var2
+sub = Var1-Var2
+multi = Var1*Var2
+div = Var1/Var2
+
+#output
+print("Addition -",add)
+print("Subtraction -",sub)
+print("Multiplication -",multi)
+print("Division -",div)
