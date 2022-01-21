6bb6dd5ee163f9bc99bf6a1d0fd8dc9ac8bc462a
Simas Simanavicius <makleriux@gmail.com>
2022-01-21 22:07:35 +0000

    Update OOP get second biggest number.py

--- OOP get second biggest number.py
+++ OOP get second biggest number.py
@@ -8,11 +8,14 @@ Write a Python program to find the second largest number in Python from the user
 list.
 
 """
-List_data = [10023, 764556, 7088, 22, 987, 980731, 999]
-
-class Yolo:
-    def __init__(self, number):
-        self.number = number.sort()
-        print(number[-2])
+List_data = [] 
+try:     #while geting integers add them to list
+    while True:
+        List_data.append(int(input()))
+except:
+    class Yolo:
+        def __init__(self, number):
+            self.number = number.sort()
+            print(number[-2])
 
 Yolo(List_data)
